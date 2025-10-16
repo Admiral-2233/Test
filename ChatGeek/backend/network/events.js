03f8a03c764784e50cf148776d923a7fed29d75c
Alex-2233 <a1913865@adelaide.edu.au>
2025-10-17 00:53:00 +1030

    NewTest

--- ChatGeek/backend/network/events.js
+++
@@ -1,9 +0,0 @@
-// Lightweight event bus for the network layer -> local layer hand-off.
-
-const { EventEmitter } = require("events");
-const bus = new EventEmitter();
-
-// Cap listener leak warnings during development.
-bus.setMaxListeners(50);
-
-module.exports = bus;
--- ChatGeek/frontend/src/utils/base64url.js
+++
@@ -1,8 +0,0 @@
-export const b64u = {
-  enc: (bytes) =>
-    btoa(String.fromCharCode(...bytes))
-      .replace(/\+/g,'-').replace(/\//g,'_').replace(/=+$/,''),
-  dec: (s) =>
-    new Uint8Array(atob(s.replace(/-/g,'+').replace(/_/g,'/'))
-      .split('').map(c => c.charCodeAt(0))),
-};
